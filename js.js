292800e401e70eb0bc70f7a048b1ab756c14a085
AMI35 <pokemon_1229dm@icloud.com>
2022-06-07 20:43:12 +0900

    add files

---
+++ js.js
@@ -0,0 +1,34 @@
+// console.log("ink");
+
+// const arr = ["one", "second", "third", "4", "hey", "mo ii", 49494]
+
+// for (let i = 0; i < 6; i++) {
+//     const element = arr [i];
+//     console.log(element);    
+// }
+
+// let j=0;
+// while (j<10) {
+//     console.log(j);
+//     j ++;
+// }
+
+
+// const asd = () => {
+//     console.log("Hello world");
+// }
+// setTimeout(asd, 5000);
+
+// for (let i = 1; i < 10; i++) {
+//     const sdf = () => {
+//         console.log(i);
+//     }
+//     setTimeout(sdf,1000*i);
+// }
+
+// let j=1
+// setInterval(() => {
+//     console.log(j++)
+// }, 
+// 1000);
+
